3cba2da1e96f87182e6e20dbe036cb97b8f57561
Antoine Correira <antoine.correira@gmail.com>
2024-11-01 12:56:15 +0100

    whatyearisit frontend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch('https://whatyearisit-backend-eight-eta.vercel.app/year')
 .then(response => response.json())
 .then(data => {
-    document.querySelector('#year').textContent = data.date;
+    document.querySelector('#year').textContent = data.year;
 })
